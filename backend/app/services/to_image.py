698b57c5952ad481969bd46b83965c606c258915
edgarLB <103465521+edgarLB@users.noreply.github.com>
2025-10-29 19:54:52 -0700

    Glow effect

--- backend/app/services/to_image.py
+++ backend/app/services/to_image.py
@@ -1,6 +1,6 @@
 import math
 
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageFilter, ImageChops
 from ascii_convert import asciiText, img
 
 outputName = 'output'
@@ -16,15 +16,39 @@ maxWidth = max(len(line) for line in lines)
 imageWidth = math.floor(maxWidth * fontSize * spacing_factor)
 imageHeight = math.floor(len(lines) * fontSize * spacing_factor)
 
-result = Image.new('RGB', (imageWidth, imageHeight), color='black')
+result = Image.new('RGBA', (imageWidth, imageHeight), color=(0, 0, 0, 0))
 draw = ImageDraw.Draw(result)
+
+glow = Image.new('RGBA', result.size, (0, 0, 0, 0))
+glowDraw = ImageDraw.Draw(glow)
 
 for i, line in enumerate(lines):
     for j, char in enumerate(line):
         # BGR to RGB
         color = (img[i][j][2], img[i][j][1], img[i][j][0])
 
-        draw.text((j * fontSize * spacing_factor, i * fontSize * spacing_factor), char, font=font, fill=color)
+        x = j * fontSize * spacing_factor
+        y = i * fontSize * spacing_factor
+
+        draw.text((x, y), char, font=font, fill=color)
+        glowDraw.text((x, y), char, font=font, fill=color, stroke_width=1, stroke_fill=color)
 
 
-result.save(f'../temp/{outputName}.png')
+#--------- Glow Effect Effect Compositing -----#
+background = Image.new('RGBA', result.size, color='black')
+
+# tight blur
+glow.putalpha(110)
+glow = glow.filter(ImageFilter.GaussianBlur(radius=2))
+background.paste(glow, (0,0), glow)
+
+# sharp text
+background.paste(result, (0,0), result)
+
+# glow
+glow.putalpha(250)
+glow = glow.filter(ImageFilter.GaussianBlur(radius=6))
+background = ImageChops.add(background, glow)
+
+
+background.save(f'../temp/{outputName}.png')
